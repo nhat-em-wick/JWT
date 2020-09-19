1c7e92d6f9920c82ef3d78068d44a650478d4245
NhatNguyen <mnhat233@gmail.com>
2020-09-19 21:21:46 +0700

    add pagination api

--- index.js
+++ index.js
@@ -10,14 +10,26 @@ const app = express();
 const port = 3500;
 
 const mongoose = require('mongoose');
+const userModel = require('./models/user.model');
+app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
-
 app.use('/api/user', authRoute);
 app.use('/api', postRoute);
+app.get('/users', async function(req, res){
+  let page = parseInt(req.query.page) || 1;
+  let perPage = 3;
+  const start = (page - 1)*perPage;
+  const end = page*perPage;
+  let totalUser = await userModel.find();
+  let totalPages = Math.ceil(totalUser.length / perPage);
 
+  let users = await (await userModel.find()).slice(start,end);
+
+  res.render('index',{users: users, totalPages: totalPages});
+})
 
 //connect mongodb
 mongoose.connect(process.env.DB_CONNECTION, function(){
